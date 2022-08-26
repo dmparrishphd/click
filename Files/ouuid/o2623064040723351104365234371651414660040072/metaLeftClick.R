3c0bcdc7c01aecb0143619d7601ed34d7633536d
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2022-08-26 08:51:27 -0400

    Delete metaLeftClick.R

--- Files/ouuid/o2623064040723351104365234371651414660040072/metaLeftClick.R
+++
@@ -1,7 +0,0 @@
-function ( pathname ) {
-    SRC <- paste0 ( pathname , "/lib/protoLeftClick.R" )
-    LIST <- source ( SRC ) [[ 1 ]] ( pathname )
-    grEvtEnv <- with (
-        LIST $ grEvtEnvMakeEnv ,
-        grEvtEnv ( HANDLERS ) )
-    function ( ... ) LIST $ FUN ( grEvtEnv , ... ) }
